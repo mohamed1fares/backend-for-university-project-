029d84bc2d3ad26d54f96aa19af06fcb0cb9f323
mohamed1fares <110127407+mohamed1fares@users.noreply.github.com>
2025-12-09 17:52:48 +0200

    .

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// const express =require('express');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db.config');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-
-// dotenv.config();
-// const app = express();
-// app.use(express.json());
-// connectDB();
-// app.use(cors());
-
-
-// // const corsmiddleware = require(`./middelwares/cors.middelware`)
-// // app.use(corsmiddleware);
-
-// app.use('/api/users', require('./routes/user.routes'));
-// app.use('/api/urls', require('./routes/url.routes'));
-// app.use('/api/vuln', require('./routes/vuln.routes'));
-// app.use('/api/results', require('./routes/results.routes'));
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server is running on port ${process.env.PORT}`);
-// });
 
 
 // server.js
@@ -51,7 +27,6 @@ app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/urls', require('./routes/url.routes'));
 app.use('/api/vuln', require('./routes/vuln.routes'));
 app.use('/api/results', require('./routes/results.routes'));
-// app.use('/api/scan', require('./routes/scan.routes'));
 
 
 // basic error handler (so multer/file errors return nice message)
--- routes/scan.routes.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const scanController = require('../controller/oldScan.controller'); // حسب مكان الملف
-
-// // الرابط: POST /api/scan/start
-// router.post('/scan/start', scanController.runDynamicScan);
-
-// module.exports = router;
