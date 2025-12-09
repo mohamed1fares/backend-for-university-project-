9f8e484cf509f2479491cd3958a8619f9ec19f8e
mohamed1fares <110127407+mohamed1fares@users.noreply.github.com>
2025-12-09 18:58:32 +0200

    .

--- routes/results.routes.js
+++ routes/results.routes.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const {Result,getResultsByUrl,getResults} = require('../controller/results.controller');
-// const {authenticate} = require('../middlewares/auth.middleware');
-// const {authorize} = require('../middlewares/role.middelware');
-
-
-// // getResults
-
-// router.get('/:id',authenticate,getResultsByUrl)
-// router.get('/',authenticate,authorize('admin'),getResults)
-// router.post('/result', authenticate, Result);
-
-// module.exports = router;
-
-
-
-
-
-
-
-// const express = require('express');
-// const router = express.Router();
-// const resultController = require('../controller/results.controller');
-// const {authenticate} = require('../middlewares/auth.middleware');
-// const {authorize} = require('../middlewares/role.middelware');
-
-// // هذا هو الرابط الجديد للفحص الشامل
-// // POST http://localhost:3000/api/results/scan-all
-// router.post('/scan-all', resultController.scanAll);
-
-// // الروابط القديمة للعرض
-// router.get('/url/:id', authenticate,resultController.getResultsByUrl);
-// router.get('/', authenticate,authorize('admin'),resultController.getAllResults);
-
-// module.exports = router;    
-
-
 
 const express = require('express');
 const router = express.Router();
@@ -44,12 +6,12 @@ const {authenticate} = require('../middlewares/auth.middleware');
 const {authorize} = require('../middlewares/role.middelware');
 
 // بدء الفحص
-router.post('/scan-all', resultController.scanAll);
+router.post('/scan-all',  authenticate, resultController.scanAll);
 
 // جلب تاريخ الفحوصات لرابط معين (History)
-router.get('/url/:id/reports', resultController.getReportsByUrl);
+router.get('/url/:id/reports',authenticate, resultController.getReportsByUrl);
 
 // جلب تفاصيل تقرير محدد (Details)
-router.get('/report/:reportId', resultController.getReportById);
+router.get('/report/:reportId',authenticate, resultController.getReportById);
 
 module.exports = router;
